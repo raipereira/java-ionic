e3005269c3941feea21bb8a5ec26f583bba5819d
Raimundo Pereira <raipereira84@gmail.com>
2021-05-05 10:21:57 -0700

    Publish Category data in the database

--- src/main/java/com/casestudy/JavaionicApplication.java
+++ src/main/java/com/casestudy/JavaionicApplication.java
@@ -1,13 +1,34 @@
 package com.casestudy;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.casestudy.model.Category;
+import com.casestudy.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class JavaionicApplication {
+public class JavaionicApplication implements CommandLineRunner {
+	
+	@Autowired
+	CategoryRepository cr;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaionicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category c1 = new Category(null, "Informatics");
+		Category c2 = new Category(null, "Office");
+		
+		cr.saveAll(Arrays.asList(c1,c2));
+		
+		
 	}
 
 }
