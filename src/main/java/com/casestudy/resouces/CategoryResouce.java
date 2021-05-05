3693da157680206871b052298e61f0c54aa1219e
Raimundo Pereira <raipereira84@gmail.com>
2021-05-04 23:26:24 -0700

    Create and mapping model Category

--- src/main/java/com/casestudy/resouces/CategoryResouce.java
+++ src/main/java/com/casestudy/resouces/CategoryResouce.java
@@ -1,16 +1,29 @@
 package com.casestudy.resouces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.casestudy.model.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResouce {
 	
 	@GetMapping
-	public String list() {
-		return "Test end-point";
+	public List<Category> list() {
+		
+		Category c1 = new Category(1, "Informatic");
+		Category c2 = new Category(2, "Office equipaments");
+		
+		List<Category> clist = new ArrayList<>();
+		clist.add(c1);
+		clist.add(c2);
+		
+		return clist;
 	}
 
 }
