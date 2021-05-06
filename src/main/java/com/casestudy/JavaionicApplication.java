56ff931f98580a12c94bca56a0c9bf0c2ad3b12e
Raimundo Pereira <raipereira84@gmail.com>
2021-05-05 21:11:03 -0700

    mapping State and City

--- src/main/java/com/casestudy/JavaionicApplication.java
+++ src/main/java/com/casestudy/JavaionicApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.casestudy.model.Category;
+import com.casestudy.model.City;
 import com.casestudy.model.Product;
+import com.casestudy.model.State;
 import com.casestudy.repositories.CategoryRepository;
+import com.casestudy.repositories.CityRepository;
 import com.casestudy.repositories.ProductRepository;
+import com.casestudy.repositories.StateRepository;
 
 @SpringBootApplication
 public class JavaionicApplication implements CommandLineRunner {
@@ -20,6 +24,12 @@ public class JavaionicApplication implements CommandLineRunner {
 	
 	@Autowired
 	ProductRepository pr;
+	
+	@Autowired
+	CityRepository ctr;
+	
+	@Autowired
+	StateRepository sr;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaionicApplication.class, args);
@@ -44,6 +54,19 @@ public class JavaionicApplication implements CommandLineRunner {
 		
 		cr.saveAll(Arrays.asList(c1,c2));
 		pr.saveAll(Arrays.asList(p1,p2,p3));
+		
+		State s1 = new State(null, "California");
+		State s2 = new State(null, "Florida");
+		
+		City ct1 = new City(null, "San Diego", s1);
+		City ct2 = new City(null, "Los Ageles", s1);
+		City ct3 = new City(null, "Miami", s2);
+		
+		s1.getCities().addAll(Arrays.asList(ct1,ct2));
+		s2.getCities().addAll(Arrays.asList(ct3));
+		
+		sr.saveAll(Arrays.asList(s1,s2));
+		ctr.saveAll(Arrays.asList(ct1,ct2,ct3));
 		
 		
 		
---
+++ src/main/java/com/casestudy/repositories/CityRepository.java
@@ -0,0 +1,11 @@
+package com.casestudy.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.casestudy.model.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Integer> {
+
+}
